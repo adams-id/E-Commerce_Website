5692fe5997a537fd654e27a83c5808a0fea9ff95
Adams-I <idreezthor@gmail.com>
2021-03-22 16:34:03 +0100

    Updating the app to riderect to homepage upon signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'; //enables us to use different react-router methods
+import {Switch, Route, Redirect} from 'react-router-dom'; //enables us to use different react-router methods
 import {connect} from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -42,7 +42,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={HomePage} /> 
           <Route path='/shop' component={shopPage} />
-          <Route path='/signin' component={SignInAndSignOut} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' /> ) : (<SignInAndSignOut />)} />
         </Switch>
       </div>
   
@@ -53,8 +53,12 @@ class App extends React.Component{
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
